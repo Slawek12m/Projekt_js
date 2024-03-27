c3187b5c840f7fdcee263b0a64800cba6a2b0701
slawomir.miller <96744586+Slawek12m@users.noreply.github.com>
2024-03-27 21:22:30 +0100

    Projekt 12  v.03 tablica

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//   ------Projekt 12  v.03 dodanie zadań----
+//   ------Projekt 12  v.03 tablica----
+const toDoList = [];
 
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
@@ -7,20 +8,22 @@ const listItems = document.getElementsByClassName('task');
 const input = document.querySelector('input');
 
 const removeTask = (e) => {
-  e.target.parentNode.remove();
+  // e.target.parentNode.remove();
+  const index = e.target.parentNode.dataset.key;
+  toDoList.splice(index, 1);
   taskNumber.textContent = listItems.length;
+  renderList();
 };
 
 const addTask = (e) => {
   e.preventDefault();
   const titleTask = input.value;
   if (titleTask === '') return;
-
   const task = document.createElement('li');
-
   task.className = 'task';
-
   task.innerHTML = titleTask + '<button>Usuń</button>';
+  toDoList.push(task);
+  renderList();
   ul.appendChild(task);
   input.value = '';
   taskNumber.textContent = listItems.length;
@@ -28,4 +31,11 @@ const addTask = (e) => {
   //   document.querySelector('li:last-child button'), addEventListener('click', removeTask);
 };
 
+const renderList = () => {
+  ul.textContent = '';
+  toDoList.forEach((toDoElement, key) => {
+    toDoElement.dataset.key = key;
+    ul.appendChild(toDoElement);
+  });
+};
 form.addEventListener('submit', addTask);
